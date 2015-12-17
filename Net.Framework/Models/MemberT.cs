b270118ac750c409d777f7177f347828c29a5117
jungil <jungil.me@gmail.com>
2015-12-17 19:24:05 +0900

    initial commit

---
+++ Net.Framework/Models/MemberT.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Net.Framework.Models
+{
+    public class MemberT
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int No { get; set; }
+
+        [Required(ErrorMessage = "ID is must be Required.")]
+        public virtual string Id { get; set; }
+        public virtual string BlogUrl { get; set; }
+        public virtual int Level { get; set; }
+        public virtual string Status { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string CellPhone { get; set; }
+        public virtual string Url { get; set; }
+        public virtual string SnsType { get; set; }
+        public virtual string SnsId { get; set; }
+
+        //[JsonConverter(typeof(StringEnumConverter))]
+    }
+}
---
+++ Net.Framework/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Net.Framework.Models;
+
+namespace Net.Framework
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<MemberT> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<IncludeMetadataConvention>();
+        }
+    }
+}
